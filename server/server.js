a35480fd88805be1ce58766181d54f49cef02d4b
“David” <arias.maulen@gmail.com>
2021-02-02 22:44:18 -0400

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -1,46 +1,28 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', {
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
+        useNewUrlParser: true,
+        useUnifiedTopology: true
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id: id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
+    })
+    .then(() => console.log('base de datos ONLINE'))
+    .catch(err => console.log('No se pudo conectar', err));
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto: ', process.env.PORT);
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
